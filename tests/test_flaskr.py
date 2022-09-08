ebc67ae2209d66b2830082fcedd93c9a51dbdc8c
Quoc Nam Tran <namquoctran24@gmail.com>
2022-09-08 20:58:50 +0200

    deleted comments

--- tests/test_flaskr.py
+++ tests/test_flaskr.py
@@ -12,34 +12,3 @@ def test_update(client):
 
 def test_delete(client):
     assert client.post('/delete').status_code == 403
-
-
-# class RoutesTest(unittest.TestCase):
-
-
-#     # Check for response 404
-#     def test_add(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/delete")
-#         statuscode = response.status_code
-#         self.assertEqual(statuscode, 404)
-
-
-#     # Check for response 404
-#     def test_delete(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/delete")
-#         statuscode = response.status_code
-#         self.assertEqual(statuscode, 404)
-
-
-#     # Check for response 200
-#     def test_update(self):
-#         tester = app.test_client(self)
-#         response = tester.get("/update")
-#         statuscode = response.status_code
-#         self.assertEqual(statuscode, 200)
-
-
-# if __name__ == "__main__":
-#     unittest.main()
